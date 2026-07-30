8d15ffdca98864a928d2d5368a5e158a5a937726
CaoDucManh1611 <caoducmanh1611@gmail.com>
2026-07-31 00:55:02 +0700

    Add PostgreSQL configuration support

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,10 @@
+import os
+
 from pydantic import BaseModel
+
 
 class Settings(BaseModel):
     project_name: str = "Book Management API"
-    sqlalchemy_database_url: str = "sqlite:///./app.db"
-    
+    sqlalchemy_database_url: str = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+
 settings = Settings()
